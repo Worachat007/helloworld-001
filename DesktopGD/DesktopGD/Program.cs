d0a2b1fce75d288a6cf9e245eaf74b67afdcf0a9
Worachat007 <worachat.pi@kkumail.com>
2024-11-26 11:20:57 +0700

    worachat's testoop

---
+++ DesktopGD/DesktopGD/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Worachat Pidtanang";
+string student_id = "673450039-8";
+// string is Datatype ->  ชนิดของข้อมูล
+// name is variable
+// "..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is worachat pidtanang");
+Console.WriteLine("My student id: 673450039-8");
